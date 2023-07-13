83757aed061aa944fa5ac60b69c8429f49bc33bb
Kundan Kumar <Kundan9811721117@gmail.com>
2023-07-13 11:19:02 +0530

    first commit

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+let BASE_URL = "https://redpost-backend.onrender.com/";
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "Development") {
+//   BASE_URL = "http://localhost:4000/";
+// }
+
+export { BASE_URL };
